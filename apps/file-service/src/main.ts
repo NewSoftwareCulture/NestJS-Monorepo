0f0045a535863bf5b794bee2d077053eed2de2aa
Vlados <vlados.pochta@gmail.com>
2024-04-13 03:15:37 +0300

    fix: remove file-service

--- apps/file-service/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { NestFactory } from '@nestjs/core';
-import { FileServiceModule } from './file-service.module';
-
-async function bootstrap() {
-  const app = await NestFactory.create(FileServiceModule);
-  await app.listen(3000);
-}
-bootstrap();
